44a8e8cf513b32e5bea85071ddd9edff954a93b3
reem <tarekreem950@gmail.com>
2023-08-09 11:39:42 -0700

    date time

--- models/base_model.py
+++ models/base_model.py
@@ -14,17 +14,20 @@ class BaseModel:
         """Initiation of new instance"""
 
         self.id = str(uuid.uuid4())
-        self.created_at =
-        self.updated_at =
-    
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
     def save(self):
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
         """Create dictionary of object"""
 
         update_dict = self.__dict__
-        update_dict['__class__'] = cls.__name__
+        update_dict['__class__'] = "BaseModel"
+        update_dict['created_at'] = self.created_at.isoformat()
+        update_dict['updated_at'] = self.updated_at.isoformat()
         return update_dict
-    
+
     def __str__(self):
-        return f"[{cls.__name__}] ({self.id}) {self.__dict__}"
+        return f"[BaseModel] ({self.id}) {self.__dict__}"
