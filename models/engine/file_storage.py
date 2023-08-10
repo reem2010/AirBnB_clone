a2e669720867f5292e05df02f5ae531de39c2279
reem <tarekreem950@gmail.com>
2023-08-10 04:19:16 -0700

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,5 +9,24 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    
+    def all(self):
+        """returns the dictionary"""
+        return (self.__objects)
 
+    def new(self, obj):
+        """sets in __objects the obj"""
+        key = f"{obj.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file"""
+        out = json.dumps(self.__objects)
+        with open(self.__file_path, "w") as file:
+            file.write(out)
+
+    def reload(self):
+        """deserializes the JSON file to __objects"""
+        with open(self.__file_path, "r") as file:
+            self.__objects = json.loads(file.read())
 
