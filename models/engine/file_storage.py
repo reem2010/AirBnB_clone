5f6a831e1a15d8beb0ac49798d207569fa64be7c
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-08-10 08:55:25 -0700

    TEST

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,7 @@
 """file storage module"""
 import json
 import os
+import models
 
 class FileStorage:
     """
@@ -13,27 +14,29 @@ class FileStorage:
     
     def all(self):
         """returns the dictionary"""
-        return (self.__objects)
+        print(f"{FileStorage.__objects}all")
+        return (FileStorage.__objects)
 
     def new(self, obj):
-        print(type(obj))
         """sets in __objects the obj"""
         key = f"BaseModel.{obj.id}"
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
+        print(f"{FileStorage.__objects}new \n {obj}")
 
     def save(self):
         """serializes __objects to the JSON file"""
-        new_dict = self.__objects.copy()
-        for value, key in new_dict.items():
-            print(type(value))
+        new_dict = FileStorage.__objects
+        for key, value in new_dict.items():
             new_dict[key] = value.to_dict()
-        out = json.dumps(self.__objects)
-        with open(self.__file_path, "w") as file:
+        out = json.dumps(new_dict)
+        print(f"{FileStorage.__objects}save")
+        with open(FileStorage.__file_path, "w") as file:
             file.write(out)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
-        if os.path.exists(self.__file_path): 
-            with open(self.__file_path, "r") as file:
-                self.__objects = json.loads(file.read())
-
+        print(f"{FileStorage.__objects}reload")
+        if os.path.exists(FileStorage.__file_path): 
+            with open(FileStorage.__file_path, 'r') as file:
+                readFile = json.loads(file.read())
+            FileStorage.__objects = {key: (readFile[key['__class__']])(**readFile[key]) for key in readFile.keys()}
